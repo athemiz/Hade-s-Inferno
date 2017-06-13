1986278039ae048911e3d873041bb82f1dc7d092
athemiz <danteps3@hotmail.com>
2017-06-13 19:07:47 -0300

    Hade's Inferno

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject barqueiro;
+    public GameObject dBox;
+    public Text dText;
+    public Text dName;
+
+    public bool dialogActive;
+
+    public int i = 0;
+    public bool binativo = false;
+    public void BarqueiroDialog()
+    {
+        string[] dialogo = new string[] { "O que um mortal como você faz aqui, na margem do rio dos mortos?",
+            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
+            "Você esta louco?",
+            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
+            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
+            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
+            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
+            "Com todo prazer!",
+            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
+        string[] nome = new string[] {"Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        if (barqueiro.transform.position.x - player.transform.position.x < 5 && !binativo) ShowBox(dialogo, nome);
+    }
+
+    public void ShowBox(string[] dialogue, string[] nome) {
+        dBox.SetActive(true);
+        dialogActive = true;
+        dText.text = dialogue[i];
+        dName.text = nome[i];
+        player.gameObject.GetComponent<PlayerController>().enabled = false;
+        if (dialogActive && Input.GetKeyDown(KeyCode.Space) && i != dialogue.Length-1)
+        {
+            i++;
+            dName.text = nome[i];
+            dText.text = dialogue[i];
+        }
+        else if(i==dialogue.Length-1 && Input.GetKeyDown(KeyCode.Space))
+        {
+            player.SetActive(true);
+            binativo = true;
+            dBox.SetActive(false);
+            dialogActive = false;
+            player.gameObject.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        BarqueiroDialog();
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+// Parametros - MOVIMENTO
+    public float velocidade;
+
+// Parametros Animação - CORRER
+    public Transform player;
+    private Animator animator;
+    public Rigidbody2D body;
+
+    // Parametros Animação - PULO
+    public float force = 100;
+
+    public float jumpTime = 0.4f;
+    public float jumpDelay = 0.4f;
+    public bool isGrounded;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = player.GetComponent<Animator> ();
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D colision)
+    {
+        if (colision.gameObject.tag == "Ground") isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D colision)
+    {
+        if (colision.gameObject.tag == "Ground") isGrounded = false;
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        Movimentar();
+    }
+
+    void Movimentar()
+    {
+// Parametros Animação - CORRER
+        animator.SetFloat("run",Mathf.Abs(Input.GetAxis("Horizontal")));
+
+// Parametros - MOVIMENTO
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+
+            transform.Translate(Vector2.right * velocidade * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+
+      
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            body.AddForce(transform.up * force);
+            jumpTime = jumpDelay;
+            animator.SetTrigger("Jump");
+        }
+
+       
+
+       if (isGrounded)
+       {
+            animator.SetTrigger("Ground");
+        }
+    }
+
+}
