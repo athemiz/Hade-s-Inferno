4f32bd995570c2baa4486641a5d12c6064b2db28
athemiz <danteps3@hotmail.com>
2017-06-21 14:42:44 -0300

    Added Menu

--- Assets/Scripts/Scenes/ScenesManager.cs
+++ Assets/Scripts/Scenes/ScenesManager.cs
@@ -1,25 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScenesManager : MonoBehaviour {
-
+    private Scene scene;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        scene = SceneManager.GetActiveScene();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("aaaa");
             Application.LoadLevel("LevelSelect");
         }
     }
 
     // Update is called once per frame
     void Update () {
-		
+        if (scene.name == "Menu" && Input.GetKeyDown(KeyCode.Space)) Application.LoadLevel("Fase 1");
 	}
 }
