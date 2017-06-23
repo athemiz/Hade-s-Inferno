8e7482b86104b648f155474633d32b36cf53727a
athemiz <danteps3@hotmail.com>
2017-06-23 20:36:17 -0300

    Dialog in every map implemented

--- Assets/Scripts/Dialogo/DialogManager.cs
+++ Assets/Scripts/Dialogo/DialogManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DialogManager : MonoBehaviour {
 
@@ -11,11 +12,16 @@ public class DialogManager : MonoBehaviour {
     public GameObject cutscenes;
     public Text dText;
     public Text dName;
+    private Scene scene;
 
     public bool dialogActive;
 
     private int i = 0;
     public bool binativo = false;
+    public bool fase2 = false;
+    public bool fase3 = false;
+    public bool fase4 = false;
+    public bool fase5 = false;
     public void BarqueiroDialog()
     {
         string[] dialogo = new string[] { "O que um mortal como você faz aqui, na margem do rio dos mortos?",
@@ -30,8 +36,65 @@ public class DialogManager : MonoBehaviour {
         string[] nome = new string[] {"Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
         if (barqueiro.transform.position.x - player.transform.position.x <= 3 && !binativo) ShowBox(dialogo, nome);
     }
+    public void Fase2Dialog()
+    {
+        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
+            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
+            "Você esta louco?",
+            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
+            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
+            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
+            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
+            "Com todo prazer!",
+            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
+        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        if(!fase2) ShowBox(dialogo, nome);
+    }
+    public void Fase3Dialog()
+    {
+        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
+            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
+            "Você esta louco?",
+            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
+            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
+            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
+            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
+            "Com todo prazer!",
+            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
+        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        if (!fase3) ShowBox(dialogo, nome);
+    }
+    public void Fase4Dialog()
+    {
+        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
+            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
+            "Você esta louco?",
+            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
+            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
+            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
+            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
+            "Com todo prazer!",
+            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
+        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        if (!fase4) ShowBox(dialogo, nome);
+    }
+    public void Fase5Dialog()
+    {
+        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
+            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
+            "Você esta louco?",
+            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
+            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
+            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
+            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
+            "Com todo prazer!",
+            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
+        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        if (!fase5) ShowBox(dialogo, nome);
+    }
 
     public void ShowBox(string[] dialogue, string[] nome) {
+        Debug.Log("hahah");
         dBox.SetActive(true);
         dialogActive = true;
         dText.text = dialogue[i];
@@ -49,21 +112,33 @@ public class DialogManager : MonoBehaviour {
         else if(i==dialogue.Length-1 && Input.GetKeyDown(KeyCode.Space))
         {
             player.SetActive(true);
-            binativo = true;
             dBox.SetActive(false);
             dialogActive = false;
             player.gameObject.GetComponent<PlayerController>().enabled = true;
-            cutscenes.SetActive(true);
+
+            if (scene.name == "Fase 1")
+            {
+                cutscenes.SetActive(true);
+                binativo = true;
+            }
+            if (scene.name == "Fase 2") fase2 = true;
+            if (scene.name == "Fase 3") fase3 = true;
+            if (scene.name == "Fase 4") fase4 = true;
+            if (scene.name == "Fase 5") fase5 = true;
         }
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        scene = SceneManager.GetActiveScene();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        BarqueiroDialog();
-	}
+        if (scene.name=="Fase 1") BarqueiroDialog();
+        if (scene.name == "Fase 2") Fase2Dialog();
+        if (scene.name == "Fase 3") Fase3Dialog();
+        if (scene.name == "Fase 4") Fase4Dialog();
+        if (scene.name == "Fase 5") Fase5Dialog();
+    }
 }
