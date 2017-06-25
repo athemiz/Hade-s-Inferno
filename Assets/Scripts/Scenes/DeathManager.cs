96b47649fc80feff1dffbabcf2c8e9de461a4824
athemiz <danteps3@hotmail.com>
2017-06-24 22:46:40 -0300

    Update
    1: som lobo -
    2: fase 3 music -
    3: fase 4 music -
    4: castelo music -
    5: add fase 5 -
    6: add castelo -
    7: increased camera -

---
+++ Assets/Scripts/Scenes/DeathManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathManager : MonoBehaviour
+{
+    private Scene scene;
+    // Use this for initialization
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(scene.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
