f16d4f88834d4e45ee27776a87b8a91e58dde464
athemiz <danteps3@hotmail.com>
2017-06-25 14:37:29 -0300

    Added Dialogs and fixed blue bug

--- Assets/Scripts/Dialogo/DialogManager.cs
+++ Assets/Scripts/Dialogo/DialogManager.cs
@@ -22,6 +22,7 @@ public class DialogManager : MonoBehaviour {
     public bool fase3 = false;
     public bool fase4 = false;
     public bool fase5 = false;
+    public bool castelo = false;
     public void BarqueiroDialog()
     {
         string[] dialogo = new string[] { "O que um mortal como você faz aqui, na margem do rio dos mortos?",
@@ -38,59 +39,38 @@ public class DialogManager : MonoBehaviour {
     }
     public void Fase2Dialog()
     {
-        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
-            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
-            "Você esta louco?",
-            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
-            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
-            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
-            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
-            "Com todo prazer!",
-            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
-        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
-        if(!fase2) ShowBox(dialogo, nome);
+        string[] dialogo = new string[] { "Bem vindo ao infernos de Cemiterio de Fogo e Colinas de Rocha. Aqui estão confinados todos aqueles que desdenham a minha existencia.",
+            "As almas dos condenados ardem eternamente no poço de lava e todas as suas lembranças são transformadas em rocha formando todas as colinas desse lugar.",
+            "Espero que sua jornada termine aqui, junto com os outros." };
+        string[] nome = new string[] { "HADES", "HADES", "HADES" };
+        if (!fase2) ShowBox(dialogo, nome);
     }
     public void Fase3Dialog()
     {
-        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
-            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
-            "Você esta louco?",
-            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
-            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
-            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
-            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
-            "Com todo prazer!",
-            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
-        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        string[] dialogo = new string[] { "Bem vindo ao inferno do Rio Estinge e do Lago de Lama. Todos que viveram suas vidas com ira e ganancia terminam aqui.",
+            "As almas dos que se foram ficam atoladas na lama impossibilidades de se libertarem enquanto as aguas violentas do rio Estige as torturam impiedosamente." };
+        string[] nome = new string[] { "HADES", "HADES" };
         if (!fase3) ShowBox(dialogo, nome);
     }
     public void Fase4Dialog()
     {
-        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
-            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
-            "Você esta louco?",
-            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
-            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
-            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
-            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
-            "Com todo prazer!",
-            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
-        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        string[] dialogo = new string[] { "Bem vindo ao Cocite, o inferno do gelo e ao Vale dos Ventos. Aqui é o destino de todos aqueles que cometeram qualquer tipo de traição.",
+            "O frio e os ventos congelantes desse inferno aprissionam as almas desse lugar.  O frio é tão intenso que nenhum vestigio de calor é encontrado aqui." };
+        string[] nome = new string[] { "HADES", "HADES"};
         if (!fase4) ShowBox(dialogo, nome);
     }
     public void Fase5Dialog()
     {
-        string[] dialogo = new string[] { "O qugfdgfde um mortal como você faz aqui, na margem do rio dos mortos?",
-            "Tenho contas a acertar com o deus do submundo, barqueiro. Me leve ate a outra margem.",
-            "Você esta louco?",
-            "Sabe dos perigos que terá que enfrentar ate chegar ao mestre Hades? Os infernos são lugares inospitos para reles mortais.",
-            "Não me interessa quais perigos terei pela frente. So terei paz quando conseguir terminar minha jornada.",
-            "Você é mesmo um tolo. Sua alma será consumida e o mestre Hades irá possui-la pela eternidade.",
-            "É um risco que estou disposto a enfrentar.", "Então, vai me levar a outra margem do rio barqueiro?",
-            "Com todo prazer!",
-            "Espero que sua estadia no inferno seja a pior possivel. Hahahahaha" };
-        string[] nome = new string[] { "Caronte", "Vioder", "Caronte", "Caronte", "Vioder", "Caronte", "Vioder", "Vioder", "Caronte", "Caronte" };
+        string[] dialogo = new string[] { "Bem vindo ao inferno de Flegetonte e Malebolge. Violencia e fraude são os principais pecados das pessoas que aqui estão aprissionadas.",
+            "As almas daqui percorrem o deserto dia após dia sem destino como forma de pagar suas atrocidades cometidas em vida." };
+        string[] nome = new string[] { "HADES", "HADES" };
         if (!fase5) ShowBox(dialogo, nome);
+    }
+    public void CasteloDialog()
+    {
+        string[] dialogo = new string[] { "Incrivel mortal, você acaba de chegar no meu castelo. Espero ansiosamente sua morte em meus aposentos. Hahaha!" };
+        string[] nome = new string[] { "HADES" };
+        if (!castelo) ShowBox(dialogo, nome);
     }
 
     public void ShowBox(string[] dialogue, string[] nome) {
@@ -124,6 +104,7 @@ public class DialogManager : MonoBehaviour {
             if (scene.name == "Fase 3") fase3 = true;
             if (scene.name == "Fase 4") fase4 = true;
             if (scene.name == "Fase 5") fase5 = true;
+            if (scene.name == "Castelo") castelo = true;
         }
     }
 
@@ -139,5 +120,6 @@ public class DialogManager : MonoBehaviour {
         if (scene.name == "Fase 3") Fase3Dialog();
         if (scene.name == "Fase 4") Fase4Dialog();
         if (scene.name == "Fase 5") Fase5Dialog();
+        if (scene.name == "Castelo") CasteloDialog();
     }
 }
